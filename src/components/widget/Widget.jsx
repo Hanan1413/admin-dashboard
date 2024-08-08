47dc60faab8c6b9a48539a473677ba83da1e22fe
Hanan Ibrahim <ihanan977@gmail.com>
2024-08-08 15:10:18 +0300

    update home components and add widget feature and its style sheet

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,104 @@
+import {
+  AccountBalanceWalletOutlined,
+  MonetizationOnOutlined,
+  ShoppingCartCheckoutOutlined,
+} from "@mui/icons-material";
+import "./widget.scss";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+
+const Widget = ({ user, order, earning, balance, type }) => {
+  let data;
+
+  // temporary
+
+  const amount = 1000;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+              color: "crimson",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "view all orders",
+        icon: (
+          < ShoppingCartCheckoutOutlined 
+            className="icon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlined
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "Balance",
+        isMoney: true,
+        link: "See detaiils",
+        icon: (
+          <AccountBalanceWalletOutlined
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percntage positive">
+          <KeyboardArrowUpOutlinedIcon />
+          {diff}%
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,24 @@
-import './home.scss';
-import SideBar from '../../components/sidebar/SideBar';
-import Navbar from '../../components/navbar/Navbar';
+import "./home.scss";
+import SideBar from "../../components/sidebar/SideBar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 const Home = () => {
   return (
-    <div className='home' >
+    <div className="home">
       <SideBar />
       <div className="homeContainer">
         <Navbar />
+
+        <div className="widgets">
+          <Widget type="user"/>
+          <Widget  type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

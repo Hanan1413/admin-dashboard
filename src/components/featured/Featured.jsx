02b3116e509330b4250372d9788420f1c0cab1d9
Hanan Ibrahim <ihanan977@gmail.com>
2024-08-11 17:29:29 +0300

    add total revnue Progressbar and Area Chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,58 @@
 import "./featured.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 const Featured = () => {
-  return <div className="feature"
-  >Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text="70" strokeWidth={5} />
+        </div>
+        <p className="title">total sales made today</p>
+        <p className="amout">420$</p>
+        <p className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          cumque suscipit, nihil adipisci vitae officiis dignissimos dolore
+          repellendus inventore et hic esse quaerat, nulla ad, minima itaque
+          velit reiciendis. Impedit.$
+        </p>
+
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount ">$12.</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,8 @@ import "./home.scss";
 import SideBar from "../../components/sidebar/SideBar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
-
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 const Home = () => {
   return (
     <div className="home">
@@ -11,10 +12,14 @@ const Home = () => {
         <Navbar />
 
         <div className="widgets">
-          <Widget type="user"/>
-          <Widget  type="order"/>
-          <Widget type="earning"/>
-          <Widget type="balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>

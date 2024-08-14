8fbb4f6efe532c10cb2fdb6b56d29f0525607e6f
Hanan Ibrahim <ihanan977@gmail.com>
2024-08-14 14:13:47 +0300

    update  single page form and links on sidebar components

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,25 +1,42 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import  {userColumns, userRows} from "../../datatablesource";
-
-
+import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
 
 const Datatable = () => {
-
-  const actionColumn = [{field: "action", headerName:"Action", width:200, renderCell:()=>{
-   return(
-    <div className="cellAction">
-        <div className="viewButton">View</div>
-        <div className="deleteButton">Delete</div>
-    </div>
-   )
-  }
-  }]
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <Link
+              to="/users/test"
+              style={{ textDecoration: "none", color: "none" }}
+            >
+              <div className="viewButton">View</div>
+            </Link>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
   return (
     <div className="datatable">
-      <div style={{ height: 400, width: "100%" }}>
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new" style={{ textDecoration: "none", color: "none" }} className="link">
+          
+           Add new
+        </Link>
+      </div>
+      <div style={{ height: 600, width: "100%", marginTop: "20px" }}>
         <DataGrid
           rows={userRows}
+          style={{ lineHeight: "normal" }}
           columns={userColumns.concat(actionColumn)}
           initialState={{
             pagination: {
@@ -28,6 +45,12 @@ const Datatable = () => {
           }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
+          sx={{
+            "& .MuiDataGrid-cell": {
+              lineHeight: "normal",
+              padding: "10px",
+            },
+          }}
         />
       </div>
     </div>
